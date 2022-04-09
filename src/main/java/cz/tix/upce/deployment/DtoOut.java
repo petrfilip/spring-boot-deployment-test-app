a928d2c1782c82d78e653354cac2cba366208122
Petr Filip <petr.filip@plus4u.net>
2022-04-09 11:11:59 +0200

    Added system properties

--- src/main/java/cz/tix/upce/deployment/DtoOut.java
+++ src/main/java/cz/tix/upce/deployment/DtoOut.java
@@ -1,5 +1,11 @@
 package cz.tix.upce.deployment;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class DtoOut {
 
+  private String fact;
 }
--- src/main/java/cz/tix/upce/deployment/ProductController.java
+++ src/main/java/cz/tix/upce/deployment/ProductController.java
@@ -1,5 +1,18 @@
 package cz.tix.upce.deployment;
 
+import com.github.javafaker.Faker;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+
+  Faker faker = new Faker();
+
+  @GetMapping
+  public DtoOut getChuckNorrisFact() {
+    String fact = faker.chuckNorris().fact();
+    return new DtoOut(fact);
+  }
 
 }
